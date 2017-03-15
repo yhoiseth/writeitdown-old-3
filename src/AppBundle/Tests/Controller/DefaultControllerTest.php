0ac91ae31a66553df38f5c45739141496891994b
Yngve Høiseth <yhoiseth@gmail.com>
2017-03-15 19:04:11 +0100

    Add failing test for login route

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Tests\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Client;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
@@ -12,7 +13,22 @@ class DefaultControllerTest extends WebTestCase
 
         $crawler = $client->request('GET', '/');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertThatResponseIsOk($client);
         $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+    }
+
+    public function testLogin()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/login');
+        $this->assertThatResponseIsOk($client);
+    }
+
+    /**
+     * @param Client $client
+     */
+    private function assertThatResponseIsOk(Client $client): void
+    {
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 }
