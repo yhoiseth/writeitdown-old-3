e2b2f1558d11363c5b7604cfe72da1d270beb23c
Yngve Høiseth <yhoiseth@gmail.com>
2017-03-18 10:58:23 +0100

    Add passing JS test

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -3,11 +3,12 @@
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\MinkContext;
 
 /**
  * Defines application features from the specific context.
  */
-class FeatureContext implements Context
+class FeatureContext extends MinkContext
 {
     /**
      * Initializes context.
@@ -16,7 +17,7 @@ class FeatureContext implements Context
      * You can also pass arbitrary arguments to the
      * context constructor through behat.yml.
      */
-    public function __construct()
+    public function __construct($entityManager)
     {
     }
 }
---
+++ features/test-js.feature
@@ -0,0 +1,5 @@
+@javascript
+Feature: Test JS
+  Scenario: Test JS
+    When I am on the homepage
+    Then I should see "Added with JS"
